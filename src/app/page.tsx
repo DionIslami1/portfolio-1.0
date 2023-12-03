fdb87a02e95c36d7cd0eaae457bbc8cc15e99978
Dion Islami <dionislami1999@gmail.com>
2023-12-03 21:25:00 +0100

    feat: create banner, add some text on it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import styles from "./page.module.scss";
 
 export default function Home() {
+
   return (
-   <div>
-    <p>dion</p>
-   </div>
-  )
+    <div className={styles.wrapper}>
+      <div className={styles.header}>HEADER</div>
+      <div className={styles.banner}>
+        <h1>Welcome.</h1>
+        <div className={styles.contentBanner}>
+        <p>
+          My name is Dion Islami, I&apos;m a full-stack developer based in
+          Pristina, Kosovo, Europe. I have developed many types of full-stack apps from
+          well know Medicine applications to Ecommerce platforms. 
+        </p>
+        <br />
+        <p>
+          I&apos;m passionate about cutting-edge, database, clean code, implementation, pixel-perfect, beautiful
+          interfaces and intuitively implemented UX.
+        </p>
+        </div>
+      </div>
+    </div>
+  );
 }
